86d949dbd36d5b788f2fddce3d5157fee0de6bc8
myles-ux <fistonrwanda@gmail.com>
2022-10-26 03:09:35 +0200

    setup converstaion route ans sidebar

---
+++ src/pages/ConversationChannelPage.tsx
@@ -0,0 +1,9 @@
+import { Outlet, useParams } from "react-router-dom";
+import { ConversationChannelPageStyle } from "../utils/styles";
+
+export const ConversationChannelPage = () => {
+  console.log(useParams());
+  return (
+    <ConversationChannelPageStyle>Channel Page</ConversationChannelPageStyle>
+  );
+};
